741647f949e06e70026caab066743b4d96eaa482
Josue RZ <robledofernando585@gmail.com>
2025-09-29 17:25:29 -0700

    Funcion de despedida

--- backend/exoplaneta.py
+++ backend/exoplaneta.py
@@ -5,3 +5,7 @@ def saludar():
 def informacion():
     print("Exoplaneta es un proyecto de ciencia ciudadana que busca descubrir exoplanetas utilizando datos de telescopios.")
     return
+
+def despedida():
+    print("Gracias por usar Exoplaneta. ¡Hasta luego!")
+    return

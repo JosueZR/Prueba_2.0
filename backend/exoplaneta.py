07339b49318b17da6c341aa4d170097bebaeefba
Juan Carlos <juan.cruz11@uabc.edu.mx>
2025-09-29 17:11:56 -0700

    Funcion Exoplanetas

--- backend/exoplaneta.py
+++ backend/exoplaneta.py
@@ -1,3 +1,7 @@
 def saludar():
     print("Bienvenido a Exoplaneta")
     return
+
+def informacion():
+    print("Exoplaneta es un proyecto de ciencia ciudadana que busca descubrir exoplanetas utilizando datos de telescopios.")
+    return
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import backend.exoplaneta
 nombre = input("Ingrese su nombre: ")
 print("Hola " + nombre)
 backend.exoplaneta.saludar()
-
+backend.exoplaneta.informacion()
